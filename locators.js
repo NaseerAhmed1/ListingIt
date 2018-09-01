fc6bc80fcdc36f937e42778e7ebaa01d13e8bc03
Naseer Ahmed <naseer.ahmed@Imzee.local>
2018-09-01 17:35:37 +0500

    city and product is set to a variable

--- locators.js
+++ locators.js
@@ -5,5 +5,7 @@ module.exports = {
     searchWord:'#headerSearch',
     searchButton:'#submit-searchmain',
     searchedListing:'#body-container>div:nth-child(1)>div>div.rel.listHandler>li',
-    totalAds:'#tabs-container>div>ul.tabs.offerseek.clr.large.fleft.tohide.rel.zi3>li:nth-child(2)>span>span.color-2.normal'
+    totalAds:'#tabs-container>div>ul.tabs.offerseek.clr.large.fleft.tohide.rel.zi3>li:nth-child(2)>span>span.color-2.normal',
+    cityName:'Lahore',
+    productSearched:'nokia'
 };
